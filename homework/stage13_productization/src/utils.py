d2eeacc450374216f574444d01be9a8c77abbeb1
Aditya8321 <131906233+Aditya8321@users.noreply.github.com>
2025-08-29 20:24:11 -0400

    Delete homework directory

--- homework/stage13_productization/src/utils.py
+++
@@ -1,7 +0,0 @@
-
-def calculate_metrics(X, y):
-    """
-    Simple function to calculate regression metrics.
-    Returns dict with mean and sum of outputs.
-    """
-    return {"mean_y": sum(y)/len(y), "sum_y": sum(y)}
